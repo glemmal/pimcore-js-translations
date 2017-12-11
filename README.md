cc9bd1d0cd08159f962d164e0e08dc0b01955142
glemmal <eliasklemm@gmail.com>
2017-12-11 01:00:22 +0100

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pimcore JS translations
+This package helps you to bring your shared translations from Pimcore
+to you Javascript frontend. It uses the Pimcore translation REST Endpoint.
+You can use the simple t function to request any translation by key.
+
+## Usage
+You can attach the t function to your window object to make it
+globally available to your Javascript source.
+The init function
+
+```javascript
+import { t, init } from 'pimcore-js-translations';
+
+// define the locale you wanna fetch
+init('de', 'en').then(() => {
+  window.t = t;
+});
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+import request from 'superagent'
+
+const url = '/webservice/rest/translations?type=website'
+
+let locale = 'en'
+let fallbackLocale = null
+
+let cachedTranslations = []
+
+const fetchFromApi = (key = null) => {
+  return request
+    .get(url)
+    .then((res) => {
+      return res.body.data
+    })
+}
+
+const init = (_locale, _fallbackLocale) => {
+  if (!_locale) {
+    throw new Error('pimcore js tranlsations: no locale defined')
+  }
+
+  locale = _locale
+  fallbackLocale = _fallbackLocale
+
+  return new Promise((resolve, reject) => {
+    if (cachedTranslations.length) {
+      return resolve(cachedTranslations)
+    }
+
+    fetchFromApi().then((translations) => {
+      cachedTranslations = translations
+      return resolve(translations)
+    })
+  })
+}
+
+const t = (key) => {
+  const translationElement = cachedTranslations.find((t) => t.key === key)
+
+  if (!translationElement) {
+    return `${key} not translated`
+  }
+
+  const { translations } = translationElement
+  const translation = translations[locale] || translations[fallbackLocale]
+
+  if (!translation) {
+    return `${key} not translated for ${locale} and ${fallbackLocale}`
+  }
+
+  return translation
+}
+
+export {
+  t,
+  init
+}
